8ccb208ceca082a61107a15b39eacadc0ad8e280
mudithnirmala <52857978+mudithnirmala@users.noreply.github.com>
2022-06-13 17:48:54 +0530

    Add files via upload

--- vRProblem.py
+++ vRProblem.py
@@ -11,12 +11,14 @@ import fileinput
 EPS = pow(10,-30)
 
 N=500
-SERVICE_TIME = 100 # SET THESE NUMBERS CORRECTLY, SERVICE_TIME- PER CUSTOMER
-LOADING_TIME = 100
-CHRG_SPEED = 100
+SERVICE_TIME = 0 # SET THESE NUMBERS CORRECTLY, SERVICE_TIME- PER CUSTOMER
+LOADING_TIME = 0
+CHRG_TIME =0 # time it takes for a full charge
 KWH_CAPACITY = 100
-RANGE =90
-KG_CAPACITY = 600
+MX_CUS = 200 # maximum customers per visit
+RANGE =88000
+KG_CAPACITY = 554
+WORK_SECS = 12*3600
 W=[] # weights of goods
 min_f = pow(10,10)
 
@@ -27,18 +29,26 @@ timeMat = []
 def addZeros(seq):
     zeros =0
     dis_from_last_zero=0
-    global MX_RANGE
+    curr_weight=0
+    cus_count=0
+    global MX_RANGE,MX_CUS,KG_CAPACITY,W
 
     new_seq = [0] # starting and ending at keels
 
     for i,v in enumerate(seq):
-        if(i ==0 or dis_from_last_zero+disMat[seq[i-1]][v] + disMat[v][0] <= RANGE): # Can visit v
+        if(i ==0 or (dis_from_last_zero+disMat[seq[i-1]][v] + disMat[v][0] <= RANGE) and curr_weight+W[v]<KG_CAPACITY and cus_count<MX_CUS ): # Can visit v
             new_seq.append(v)
             dis_from_last_zero+=disMat[seq[i-1]][v]
+
+            curr_weight+= W[v]
+            cus_count+=1
         else: 
             new_seq.append(0)
             new_seq.append(v)
             dis_from_last_zero=disMat[0][v]
+
+            cus_count =1
+            curr_weight=W[v]
 
     return new_seq+[0]
 
@@ -67,27 +77,38 @@ def cleanSequence(seq):
 
     return new_seq
 
-def getTotalTime(sq):
-    global timeMat,disMat
-
-    td=0 # total distance
+def getTotalDis(sq):
+    global disMat
+    td=0
     seq =addZeros(sq)
     for i in range(1,len(seq)):
         td+= disMat[seq[i-1]][seq[i]]
-        
-    tt=LOADING_TIME # + (td/90)*charging_time
+        print(td)
 
+def getTotalDisTime(sq):
+    global timeMat,disMat,LOADING_TIME,SERVICE_TIME,CHRG_TIME,N
+
+    td=0 # total distance
+    #print("GetTotalDisTime")
+    seq =addZeros(sq)
+    #print(sq,seq)
+    for i in range(1,len(seq)):
+        td+= disMat[seq[i-1]][seq[i]]
+        
+    tt=0
+    tt_c=LOADING_TIME*(len(sq)-N) + SERVICE_TIME*(N-1) + int((max(td-RANGE,0)/RANGE)*CHRG_TIME) # constant time
+    
     for i in range(1,len(seq)):
         tt+= timeMat[seq[i-1]][seq[i]] # service time and laoding time not included- no point of adding service time.constant for all cases
 
-    return tt
+    return [td,tt,tt+tt_c]
 
 
 class Population:
     @staticmethod
     def mutate(offspring,n):
        # cnt = random.randint(0,5)
-        if random.random() < 0.5:
+        if random.random() < 0.7: 
         #  for i in range(cnt):
             i1 = random.randint(0,n-1)
             i2 = random.randint(0,n-1)
@@ -98,10 +119,10 @@ class Population:
 
     @staticmethod
     def get_fitness(creature,n):
-        return getTotalTime(creature)
+        return getTotalDisTime(creature)[0]
 
     @staticmethod
-    def crossover(c1,c2,n):
+    def crossover_1(c1,c2,n):
         ll = random.randint(1,n)
         offspring = [0]*(n)
         for i in range(ll):
@@ -109,6 +130,27 @@ class Population:
         for i in range(ll,n):
             offspring[i]= c2[i]
         return Population.mutate(cleanSequence(offspring),n)
+    
+    @staticmethod
+    def crossover_2(c1,c2,n):
+        a = random.randint(0,n-2)
+        b = min(a + random.randint(int(n/6),int(2*n/3)),n-1)
+
+        offspring = []
+
+        mid_set = c2[a:b+1]
+        free_des = [1]*(n+1)
+
+        for x in mid_set:
+            free_des[x]=0
+
+        for i in range(0,n):
+            if(free_des[c1[i]]):
+                offspring.append(c1[i])
+                free_des[c1[i]]=0
+        offspring = offspring[:a]+mid_set+ offspring[a:]
+
+        return Population.mutate(offspring,n)
 
     def build_probability(self):
         global min_f
@@ -120,7 +162,7 @@ class Population:
             f = Population.get_fitness(c,self.n) # total journey time
             min_f = min(f,min_f)
             self.fitness.append(f)
-            prob = 1/(f+EPS-min_f+500) #pow(1.5,-f) # more conflicts-> less probability
+            prob = 1/max(10,f+EPS-min_f+200) #pow(1.5,-f) # more conflicts-> less probability
             probs.append(prob)
             prob_den += prob
         self.probs = list(map(lambda x:x/prob_den,probs))
@@ -158,7 +200,7 @@ class Population:
         for _ in range(size):
             c1 = self.get_stochastic()
             c2 = self.get_stochastic()
-            offs = Population.crossover(c1,c2,self.n)
+            offs = Population.crossover_2(c1,c2,self.n)
             n_crs.append(offs)
         return Population(n_crs)
 
@@ -169,12 +211,16 @@ class Population:
             if self.fitness[i] < best_val:
                 best_val = self.fitness[i]
                 best_idx = i
-        return ( addZeros(self.creatures[best_idx]),best_val)
+        return (self.creatures[best_idx],best_val)
+        #return ([],best_val)
 
     def print_stats(self):
         avg_fitness = (sum(self.fitness) + 1.0)/len(self.fitness)
-        print('ABG Weakness: ' + str(avg_fitness)) 
-        print(self.get_best()) 
+        #print('ABG Weakness: ' + str(avg_fitness)) 
+        #print(self.get_best()[1])
+        #print(self.get_best()[0])
+        print("Dis Time :",getTotalDisTime(self.get_best()[0])) 
+        print("Number of Vehicles : ",int(getTotalDisTime(self.get_best()[0])[-1]/WORK_SECS) +1 )
   
     def get_avg(self):
         return (sum(self.fitness) + 1.0)/len(self.fitness)
@@ -186,11 +232,12 @@ def getInput():
     global disMat,timeMat
 
 
-    with open('input.txt','r') as file:
+    with open('input_full.txt','r') as file:
         N = int(file.readline().strip())
-        SERVICE_TIME,LOADING_TIME, KG_CAPACITY,CHRG_SPEED,RANGE = map(int,file.readline().strip().split())
+        #SERVICE_TIME,LOADING_TIME, KG_CAPACITY,CHRG_SPEED,RANGE = map(int,file.readline().strip().split())
 
         W = list(map(int,file.readline().strip().split()))
+        if(len(W)<=N): W=[0]*(N-len(W)+2)+W # add zero at the beginning
 
         for row in range(N):
             disMat.append(list(map(int,file.readline().strip().split())))
@@ -205,7 +252,7 @@ def getInput():
 
 if __name__ == '__main__':
     random.seed(666)
-    p_size = 50000 #f  larger the population higher chance of finding local min/max, but program becomes slow
+    p_size = 1000 #f  larger the population higher chance of finding local min/max, but program becomes slow
 
     getInput()
 
